3dc6ddb14c57d16df5fddf75861b563df1dc5427
Adam PM <adam.pm77@gmail.com>
2022-12-05 23:00:34 +0700

    perbaikan imnport siswa

--- m_spinner.php
+++ m_spinner.php
@@ -6,245 +6,248 @@ include "configuration/config_include.php";
 include "configuration/config_all_stat.php";
 
 ?>
+
 <head>
-        <meta charset="utf-8" />
-        <title>Loading |<?php echo $app;?></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Aplikasi Kelola Sales dan Keuangan" name="description" />
-        <meta content="Coderthemes" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
-        <link href="assets/libs/spinkit/spinkit.css" rel="stylesheet" type="text/css" >
-<?php
-connect();
-head();timing();
-session();
-?>
+  <meta charset="utf-8" />
+  <title>Loading |<?php echo $app; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="Aplikasi Kelola Sales dan Keuangan" name="description" />
+  <meta content="Coderthemes" name="author" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- App favicon -->
+  <link rel="shortcut icon" href="assets/images/favicon.ico">
+  <link href="assets/libs/spinkit/spinkit.css" rel="stylesheet" type="text/css">
+  <?php
+  connect();
+  head();
+  timing();
+  session();
+  ?>
 
-<?php
+  <?php
 
-if (!login_check()) {
-?>
-<meta http-equiv="refresh" content="0; url=logout" />
-<?php
-exit(0);
-}
-?>
+  if (!login_check()) {
+  ?>
+    <meta http-equiv="refresh" content="0; url=logout" />
+  <?php
+    exit(0);
+  }
+  ?>
 
-<?php
-body();
-theader();
-etc();
-
-
-//Setting Halaman
-
-error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
-include "configuration/config_chmod.php";
-
-$halaman = "kosong"; // halaman
-$dataapa = "sedang proses.."; // data
-$tabeldatabase = "kosong"; // tabel database
-$chmod = $chmenu4; // Hak akses Menu
-$forward = mysqli_real_escape_string($conn, $tabeldatabase); // tabel database
-$forwardpage = mysqli_real_escape_string($conn, $halaman); // halaman
+  <?php
+  body();
+  theader();
+  etc();
 
 
-//End Setting Halaman
- 
-?>
+  //Setting Halaman
 
-<?php
+  error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+  include "configuration/config_chmod.php";
 
-menu();
+  $halaman = "kosong"; // halaman
+  $dataapa = "sedang proses.."; // data
+  $tabeldatabase = "kosong"; // tabel database
+  $chmod = $chmenu4; // Hak akses Menu
+  $forward = mysqli_real_escape_string($conn, $tabeldatabase); // tabel database
+  $forwardpage = mysqli_real_escape_string($conn, $halaman); // halaman
 
-?>
+
+  //End Setting Halaman
+
+  ?>
+
+  <?php
+
+  menu();
+
+  ?>
 
 
 
 
-<!-- Letak Kode PHP atas -->
+  <!-- Letak Kode PHP atas -->
 
-<!-- END Letak Kode PHP atas -->
+  <!-- END Letak Kode PHP atas -->
 
 
 
 
 
   <!-- ============================================================== -->
-            <!-- Start Page Content here -->
-            <!-- ============================================================== -->
+  <!-- Start Page Content here -->
+  <!-- ============================================================== -->
 
 
-            <div class="content-page">
-                <div class="content">
-                    
-                    <!-- Start Content-->
-                    <div class="container-fluid">
-                        
-                        <!-- halaman dan breadcrumbs -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="index">DashBoard</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Pengaturan</a></li>
-                                            <li class="breadcrumb-item active"><?php echo $dataapa;?></li>
-                                        </ol>
-                                    </div>
-                                    <h4 class="page-title"><?php echo $dataapa;?></h4>
-                                </div>
-                            </div>
-                        </div>     
-                        <!-- end halaman dan breadcrumbs --> 
+  <div class="content-page">
+    <div class="content">
+
+      <!-- Start Content-->
+      <div class="container-fluid">
+
+        <!-- halaman dan breadcrumbs -->
+        <div class="row">
+          <div class="col-12">
+            <div class="page-title-box">
+              <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                  <li class="breadcrumb-item"><a href="index">DashBoard</a></li>
+                  <li class="breadcrumb-item"><a href="javascript: void(0);">Pengaturan</a></li>
+                  <li class="breadcrumb-item active"><?php echo $dataapa; ?></li>
+                </ol>
+              </div>
+              <h4 class="page-title"><?php echo $dataapa; ?></h4>
+            </div>
+          </div>
+        </div>
+        <!-- end halaman dan breadcrumbs -->
 
 
-<!-- ISI HALAMAN -->
+        <!-- ISI HALAMAN -->
 
 
-                        	 <div class="row">
-                            <div class="col-12">
-                                                                                                    
-
-                                    <div class="table-responsive">
-                                        
-                                    <table width="100%" align="center" cellspacing="0">
-                                    <tr>
-                                      <td height="500px" align="center" align="middle">
-      
-                                        <div class="sk-wave">
-                                        <div class="sk-rect sk-rect1"></div>
-                                        <div class="sk-rect sk-rect2"></div>
-                                        <div class="sk-rect sk-rect3"></div>
-                                        <div class="sk-rect sk-rect4"></div>
-                                        <div class="sk-rect sk-rect5"></div>
-                                      
-                                    </div>
-
-                                    <div >
-                                            <b>Sedang Proses, Jangan tutup halaman ini</b>
-                                    </div>
-                                    </td>
-                                    </tr>
-
-                                   
-                                    </table>
+        <div class="row">
+          <div class="col-12">
 
 
-                                    </div>
-                               
-                            </div>
-                        </div>
-                     
+            <div class="table-responsive">
+
+              <table width="100%" align="center" cellspacing="0">
+                <tr>
+                  <td height="500px" align="center" align="middle">
+
+                    <div class="sk-wave">
+                      <div class="sk-rect sk-rect1"></div>
+                      <div class="sk-rect sk-rect2"></div>
+                      <div class="sk-rect sk-rect3"></div>
+                      <div class="sk-rect sk-rect4"></div>
+                      <div class="sk-rect sk-rect5"></div>
+
+                    </div>
+
+                    <div>
+                      <b>Sedang Proses, Jangan tutup halaman ini</b>
+                    </div>
+                  </td>
+                </tr>
 
 
-   <!-- END ISI HALAMAN -->
+              </table>
 
-
-                        
-                    </div> <!-- end container-fluid -->
-
-                </div> <!-- end content -->
-<!--FOOTER-->
-                
-<?php footer();?>
-
-<!-- END FOOTER-->
 
             </div>
 
-
-
-              <!-- ============================================================== -->
-            <!-- End Page content -->
-            <!-- ============================================================== -->
+          </div>
+        </div>
 
 
 
+        <!-- END ISI HALAMAN -->
 
 
 
-<!-- Sidebar Kanan -->
-<?php
+      </div> <!-- end container-fluid -->
 
-right();
+    </div> <!-- end content -->
+    <!--FOOTER-->
 
-?>
+    <?php footer(); ?>
 
-<!-- End Sidebar Kanan -->
+    <!-- END FOOTER-->
 
-
-
-
-
-<!-- Letak Kode PHP Bawah -->
+  </div>
 
 
 
-
-<?php
-
-if(isset($_GET['import'])){
-
-     echo "<script type='text/javascript'>window.location = 'm_student_act?import=1';</script>";
-}
-
-?>
-
-
-<!-- END Letak Kode PHP bawah -->
+  <!-- ============================================================== -->
+  <!-- End Page content -->
+  <!-- ============================================================== -->
 
 
 
 
-<!-- Library & Pluggins-->
+
+
+  <!-- Sidebar Kanan -->
+  <?php
+
+  right();
+
+  ?>
+
+  <!-- End Sidebar Kanan -->
+
+
+
+
+
+  <!-- Letak Kode PHP Bawah -->
+
+
+
+
+  <?php
+
+  if (isset($_GET['import'])) {
+
+    echo "<script type='text/javascript'>window.location = 'm_student_act?import=1';</script>";
+  }
+
+  ?>
+
+
+  <!-- END Letak Kode PHP bawah -->
+
+
+
+
+  <!-- Library & Pluggins-->
   <!-- Vendor js -->
-        <script src="assets/js/vendor.min.js"></script>
+  <script src="assets/js/vendor.min.js"></script>
 
-        <script src="assets/libs/switchery/switchery.min.js"></script>
-        <script src="assets/libs/bootstrap-tagsinput/bootstrap-tagsinput.min.js"></script>
-        <script src="assets/libs/select2/select2.min.js"></script>
-        <script src="assets/libs/jquery-mockjax/jquery.mockjax.min.js"></script>
-        <script src="assets/libs/autocomplete/jquery.autocomplete.min.js"></script>
-        <script src="assets/libs/bootstrap-select/bootstrap-select.min.js"></script>
-        <script src="assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
-        <script src="assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
-        <script src="assets/libs/bootstrap-filestyle2/bootstrap-filestyle.min.js"></script>
+  <script src="assets/libs/switchery/switchery.min.js"></script>
+  <script src="assets/libs/bootstrap-tagsinput/bootstrap-tagsinput.min.js"></script>
+  <script src="assets/libs/select2/select2.min.js"></script>
+  <script src="assets/libs/jquery-mockjax/jquery.mockjax.min.js"></script>
+  <script src="assets/libs/autocomplete/jquery.autocomplete.min.js"></script>
+  <script src="assets/libs/bootstrap-select/bootstrap-select.min.js"></script>
+  <script src="assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+  <script src="assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
+  <script src="assets/libs/bootstrap-filestyle2/bootstrap-filestyle.min.js"></script>
 
-         <script src="assets/libs/moment/moment.min.js"></script>
-        <script src="assets/libs/bootstrap-colorpicker/bootstrap-colorpicker.min.js"></script>
-        <script src="assets/libs/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
-        <script src="assets/libs/clockpicker/bootstrap-clockpicker.min.js"></script>
-      
-
-      <!-- Daterange dan Select2-->
-
-       <script src="assets/datepicker/bootstrap-datepicker.js"></script>
-         <script src="assets/daterangepicker/daterangepicker.js"></script>
-            <script src="assets/libs/select2/select2.min.js"></script>
-
-              <script src="assets/libs/sweetalert2/sweetalert2.min.js"></script>
-
-        <script src="assets/js/pages/sweet-alerts.init.js"></script>
+  <script src="assets/libs/moment/moment.min.js"></script>
+  <script src="assets/libs/bootstrap-colorpicker/bootstrap-colorpicker.min.js"></script>
+  <script src="assets/libs/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+  <script src="assets/libs/clockpicker/bootstrap-clockpicker.min.js"></script>
 
 
-         <!-- Init js-->
-        <script src="assets/js/pages/form-pickers.init.js"></script>
+  <!-- Daterange dan Select2-->
 
-        <!-- Init js-->
-        <script src="assets/js/pages/form-advanced.init.js"></script>
+  <script src="assets/datepicker/bootstrap-datepicker.js"></script>
+  <script src="assets/daterangepicker/daterangepicker.js"></script>
+  <script src="assets/libs/select2/select2.min.js"></script>
 
-        <!-- App js -->
-        <script src="assets/js/app.min.js"></script>
+  <script src="assets/libs/sweetalert2/sweetalert2.min.js"></script>
 
-<!-- END Lib & Plugins-->
+  <script src="assets/js/pages/sweet-alerts.init.js"></script>
 
 
+  <!-- Init js-->
+  <script src="assets/js/pages/form-pickers.init.js"></script>
+
+  <!-- Init js-->
+  <script src="assets/js/pages/form-advanced.init.js"></script>
+
+  <!-- App js -->
+  <script src="assets/js/app.min.js"></script>
+
+  <!-- END Lib & Plugins-->
 
 
 
 
-</body>
+
+
+  </body>
+
 </html>
--- m_student_act.php
+++ m_student_act.php
@@ -7,16 +7,16 @@
 // Load file koneksi.php
 include "configuration/config_connect.php";
 
-if(isset($_GET['import'])){ // Jika user mengklik tombol Import
+if (isset($_GET['import'])) { // Jika user mengklik tombol Import
 	// Load librari PHPExcel nya
-ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
+	ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
 
 	require_once 'assets/PHPExcel/PHPExcel.php';
 
 	$inputFileType = 'CSV';
 	$inputFileName = 'tmp/data.csv';
 
-	$reader = PHPExcel_IOFactory::createReader($inputFileType);
+	$reader = PHPExcel_IOFactory::createReader($inputFileType)->setDelimiter(';');
 	$excel = $reader->load($inputFileName);
 
 	$numrow = 1;
@@ -25,7 +25,7 @@ ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
 		// Cek $numrow apakah lebih dari 1
 		// Artinya karena baris pertama adalah nama-nama kolom
 		// Jadi dilewat saja, tidak usah diimport
-		if($numrow > 1){
+		if ($numrow > 1) {
 			// START -->
 			// Skrip untuk mengambil value nya
 			$cellIterator = $row->getCellIterator();
@@ -38,35 +38,36 @@ ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
 			// <-- END
 
 			// Ambil data value yang telah di ambil dan dimasukkan ke variabel $get
-							
-                             $nis = $get[0];
-                            $nisn = $get[1]; // Ambil data nama
-                            $nama = $get[2]; // Ambil data hbeli
-                            $gender = $get[3]; // Ambil data hjual
-                            $tempat = $get[4]; // Ambil data alamat
-                            $tgllahir = date('Y-m-d',strtotime($get[5])); // Ambil data NIS
-                            $hobi = $get[6]; // Ambil data nama
-                            $nohp = $get[7]; // Ambil data jenis kelamin
-                            $ibu = $get[8]; // Ambil data telepon
-                           
-                            $ayah = $get[9]; // Ambil data NIS
-                            $waortu= $get[10]; // Ambil data nama
-                           
-                            $alamat = $get[11];
-                             $kelas = $get[12];
+
+			$nis = $get[0];
+			$nisn = $get[1]; // Ambil data nama
+			$nama = $get[2]; // Ambil data hbeli
+			$gender = $get[3]; // Ambil data hjual
+			$tempat = $get[4]; // Ambil data alamat
+			$tgllahir = date('Y-m-d', strtotime($get[5])); // Ambil data NIS
+			$hobi = $get[6]; // Ambil data nama
+			$nohp = $get[7]; // Ambil data jenis kelamin
+			$ibu = $get[8]; // Ambil data telepon
+
+			$ayah = $get[9]; // Ambil data NIS
+			$waortu = $get[10]; // Ambil data nama
+
+			$alamat = $get[11];
+			$kelas = $get[12];
+			$email = $get[13];
 			// Cek jika semua data tidak diisi
-							if($nis == "" && $nisn == "" && $nama == "" && $gender == "" && $tempat == "" && $tgllahir == "" && $nohp == "" && $ibu == "" && $ayah == "" && $waortu == "" && $alamat == "" && $kelas == "")
-								continue; // Lewat data pada baris ini (masuk ke looping selanjutnya / baris selanjutnya)
+			if ($nis == "" && $nisn == "" && $nama == "" && $gender == "" && $tempat == "" && $tgllahir == "" && $nohp == "" && $ibu == "" && $ayah == "" && $waortu == "" && $alamat == "" && $kelas == "" && $email == "")
+				continue; // Lewat data pada baris ini (masuk ke looping selanjutnya / baris selanjutnya)
 
 
-$now=date('Y-m-d');
-$password="123456";
-$pass=sha1(md5($password));
-$avatar="image/placeholder.png";
+			$now = date('Y-m-d');
+			$password = "123456";
+			$pass = sha1(md5($password));
+			$avatar = "image/placeholder.png";
 
 			// Tambahkan value yang akan di insert ke variabel $query
 			// Buat query Insert
-			$query = "INSERT INTO student VALUES('','".$nis."','".$nisn."','".$pass."','".$nama."','".$gender."','".$tempat."','".$tgllahir."','".$avatar."','".$nohp."','".$hobi."','".$alamat."','".$ibu."','".$ayah."','".$waortu."','".$kelas."','".$kelas."','','active','".$now."','".$now."')";
+			$query = "INSERT INTO student VALUES('','" . $nis . "','" . $nisn . "','" . $pass . "','" . $nama . "','" . $gender . "','" . $tempat . "','" . $tgllahir . "','" . $avatar . "','" . $nohp . "','" . $hobi . "','" . $alamat . "','" . $ibu . "','" . $ayah . "','" . $waortu . "','" . $kelas . "','" . $kelas . "','','active','" . $now . "','" . $now . "','" . $email . "')";
 			mysqli_query($conn, $query);
 		}
 
